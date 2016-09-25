c9ce8d01e573a64788e7cde213a0c442b0c8f403
Teixe <344093176@qq.com>
2016-09-25 09:26:09 +0800

    312
    312

---
+++ js/21.js
@@ -0,0 +1,12 @@
+//事件绑定函数，兼容浏览器差异
+function addEvent(element, event, listener) {
+    if (element.addEventListener) {
+        element.addEventListener(event, listener, false);
+    }
+    else if (element.attachEvent) {
+        element.attachEvent("on" + event, listener);
+    }
+    else {
+        element["on" + event] = listener;
+    }
+}
